0ff99b19ef5a606443f0071b09c38328294ce192
brasilia <ksuspec@gmail.com>
2018-06-25 21:25:13 -0300

    Alguns pequenos ajustes

--- main.cpp
+++ main.cpp
@@ -90,7 +90,6 @@ bool ReadFile(const char *filename, vector<VectorMult> *inVectors, int *maxSize,
         fprintf(stderr, "File \"%s\" could not be opened. Check file path.", filename);
         return false;
     }
-
     //Buffers
     const int LINE_MAX_LENGTH = 100000;
     char line[LINE_MAX_LENGTH];
@@ -127,14 +126,7 @@ bool ReadFile(const char *filename, vector<VectorMult> *inVectors, int *maxSize,
         }
         *maxSize = max(*maxSize, (int)((*inVectors)[nlines].axes[0]->size()) );
         nlines++; //next line
-//        for (int i = 0; i < (int)((*inVectors)[nlines-1].axes[0]->size()); i++){
-//            cout << (*(*inVectors)[nlines-1].axes[0])[i] << endl;
-//        }
-//        cout << endl<<endl;
     }
-
-
-
     fclose(fin);
     return true;
 }
@@ -161,67 +153,20 @@ void PrintDataset(vector<VectorMult> inVectors, int dimensions){
     }
 }
 
-float PointDist(float m, float n){
-    return abs(m-n);
-    //return pow((m-n),2);
-}
-
 //Calculates point distance between multidimensional vectors, either dependent or independent
 float PointDist(VectorMult s1, VectorMult s2, int p1, int p2, int dim, bool dependent = false){
     float sum = 0;
     if (dependent){
             for (int i = 0; i < dim; i++){
-            sum+= pow( (*s1.axes[i])[p1] - (*s2.axes[i])[p2], 2 );
+            sum+= pow( (*s1.axes[i])[p1] - (*s2.axes[i])[p2], 2 ); //Squared Euclidean
         }
         return sqrt(sum);
     } else {
         for (int i = 0; i < dim; i++){
-            sum+= abs((*s1.axes[i])[p1] - (*s2.axes[i])[p2]);
+            sum+= abs((*s1.axes[i])[p1] - (*s2.axes[i])[p2]); //Manhattan
         }
         return sum;
     }
-}
-
-float DTWDistance(vector<float> m, vector<float> n, float** warp){
-    //mSize+1 rows, nSize+1 cols
-    int mSize = m.size()+1;
-    int nSize = n.size()+1;
-    //Makes path
-    int i, j;
-    for (i = 1; i < mSize; i++){
-        for (j = 1; j < nSize; j++){
-            float cost = PointDist(m[i-1], n[j-1]);
-            warp[i][j] = cost + min(warp[i-1][j-1], min(warp[i][j-1], warp[i-1][j]));
-        }
-    }
-    float distance = warp[i-1][j-1];
-    return distance;
-}
-
-//With Sakoe-Chiba band
-float DTWDistance(vector<float> m, vector<float> n, float** warp, int band){
-    //mSize+1 rows, nSize+1 cols
-    int mSize = m.size()+1;
-    int nSize = n.size()+1;
-    int bandW = (band * max(mSize, nSize))/100;
-    //Crops bigger series so that the final corner can be reached
-    int maxSize = mSize + bandW;
-    mSize = min (maxSize, mSize);
-    nSize = min (maxSize, nSize);
-    //Sets out-of-range cells to infinity
-    MaxWarpM(warp, mSize, nSize, bandW);
-    //Makes path
-    int y, x;
-    for (int i = 1; i < mSize; i++){
-        for (int j = max(1, i-bandW); j <= min(nSize-1, i+bandW); j++){
-            float cost = PointDist(m[i-1], n[j-1]);
-            warp[i][j] = cost + min(warp[i-1][j-1], min(warp[i][j-1], warp[i-1][j]));
-            y = i;
-            x = j;
-        }
-    }
-    float distance = warp[y][x];
-    return distance/(mSize);
 }
 
 //Multidimensional, no band
@@ -229,23 +174,16 @@ float DTWDistance(VectorMult s1, VectorMult s2, int dim, float** warp){
     //mSize+1 rows, nSize+1 cols
     int mSize = s1.axes[0]->size()+1;
     int nSize = s2.axes[0]->size()+1;
-//    cout << "mSize: " << mSize << endl;
-//    cout << "nSize: " << nSize << endl;
-//    cout << s1.axes[0]->size() << " " << s1.axes[1]->size() << " " << s1.axes[2]->size() << " " << endl;
-//    cout << s2.axes[0]->size() << " " << s2.axes[1]->size() << " " << s2.axes[2]->size() << " " << endl;
     //Makes path
     int i, j;
     for (i = 1; i < mSize; i++){
         for (j = 1; j < nSize; j++){
             float cost = PointDist(s1, s2, i-1, j-1, dim, false);
-//            cout << "cost "<< cost<<endl;
-//            getchar();
             warp[i][j] = cost + min(warp[i-1][j-1], min(warp[i][j-1], warp[i-1][j]));
         }
     }
     float distance = warp[i-1][j-1];
-    //cout << distance <<endl;
-    return distance;
+    return distance/min(mSize, nSize);
 }
 
 //Sakoe-Chiba Band, Multidimensional
@@ -274,7 +212,7 @@ float DTWDistance(VectorMult s1, VectorMult s2, int dim, float** warp, int band)
         }
     }
     float distance = warp[y][x];
-    return distance/(mSize);
+    return distance/min(mSize, nSize);
 }
 
 //Runs test dataset against train dataset and returns accuracy
@@ -287,7 +225,7 @@ float DTWTest(vector<VectorMult> trainVectors, vector<VectorMult> testVectors, f
         for (unsigned int j = 0; j < trainVectors.size(); j++){ //templates
             //float dist = DTWDistance(*trainVectors[j].axes[0], *testVectors[i].axes[0], warpM);
             //float dist = DTWDistance(*trainVectors[j].axes[0], *testVectors[i].axes[0], warpM, 20);
-            float dist = DTWDistance(trainVectors[j], testVectors[i], dimensions, warpM, 20);
+            float dist = DTWDistance(trainVectors[j], testVectors[i], dimensions, warpM, 40);
             if (dist < minDist){
                 minDist = dist;
                 nearestNClass = trainVectors[j].seriesClass;
@@ -312,7 +250,7 @@ int main (int argc, char** argv){
     int maxSizeH = 0, maxSizeW = 0;
     vector<VectorMult> trainVectors;
     vector<VectorMult> testVectors;
-    if(!ReadFile("teste3D.txt", &trainVectors, &maxSizeH, dimensions)){
+    if(!ReadFile("treino3D.txt", &trainVectors, &maxSizeH, dimensions)){
         return -1;
     };
     if(!ReadFile("teste3D.txt", &testVectors, &maxSizeW, dimensions)){
